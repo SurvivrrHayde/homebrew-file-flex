28f90b01c305918f88d1d4398842d47da86b7e0b
Matthew Haid <90876272+SurvivrrHayde@users.noreply.github.com>
2024-06-19 18:55:58 -0700

    Update file-flex.rb

--- Formula/file-flex.rb
+++ Formula/file-flex.rb
@@ -1,16 +1,16 @@
-class CrossPlatformFsUtility < Formula
+class FileFlex < Formula
   desc "Cross-platform filesystem utility"
-  homepage "https://example.com"
+  homepage "https://github.com/SurvivrrHayde/file-flex"
   url "https://github.com/SurvivrrHayde/file-flex/releases/download/v1.0.0/file_flex.tar.gz"
   version "1.0.0"
   sha256 "b6e23f284aab6a8025255bf7463eb8fdab9ff6c3e57aa653c1406b313d1337b0"
 
   def install
-    bin.install "bin/cross_platform_fs_utility"
-    bash_completion.install "scripts/cross_platform_fs_utility-completion.sh"
+    bin.install "bin/file-flex"
+    bash_completion.install "scripts/file-flex-completion.sh"
   end
 
   test do
-    system "#{bin}/cross_platform_fs_utility", "help"
+    system "#{bin}/file-flex", "help"
   end
 end
